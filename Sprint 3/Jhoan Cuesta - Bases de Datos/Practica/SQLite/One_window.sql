8f04664e890c0634404e490f93bfb2d76df5d7a6
jhoan-debug <jhoancuesta034@gmail.com>
2024-10-06 21:46:57 -0500

    Add files via upload

---
+++ Sprint 3/Jhoan Cuesta - Bases de Datos/Practica/SQLite/One_window.sql
@@ -0,0 +1,102 @@
+--
+-- Archivo generado con SQLiteStudio v3.4.4 el dom. oct. 6 20:16:00 2024
+--
+-- Codificación de texto usada: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Tabla: Comentarios
+CREATE TABLE IF NOT EXISTS Comentarios (
+    ID_comentario    INTEGER PRIMARY KEY AUTOINCREMENT,
+    Comentario       TEXT,
+    Fecha_comentario REAL,
+    ID_solicitud     INTEGER REFERENCES Solicitud (ID_solicitud),
+    ID_encargado     INTEGER REFERENCES Encargados (ID_encargado) 
+);
+
+
+-- Tabla: Datos_usuario
+CREATE TABLE IF NOT EXISTS Datos_usuario (
+    Usuario    NUMERIC REFERENCES Usuarios (ID),
+    Contraseña TEXT
+);
+
+
+-- Tabla: Documentos
+CREATE TABLE IF NOT EXISTS Documentos (
+    ID_documento   INTEGER PRIMARY KEY AUTOINCREMENT
+                           UNIQUE,
+    Tipo           BLOB,
+    Nombre         TEXT,
+    Descripción    TEXT,
+    Fecha_creación REAL
+);
+
+
+-- Tabla: Encargados
+CREATE TABLE IF NOT EXISTS Encargados (
+    ID_encargado INTEGER PRIMARY KEY AUTOINCREMENT,
+    Nombres      TEXT,
+    Apellido     TEXT,
+    Rol          TEXT
+);
+
+INSERT INTO Encargados (
+                           ID_encargado,
+                           Nombres,
+                           Apellido,
+                           Rol
+                       )
+                       VALUES (
+                           1,
+                           'Rosa',
+                           'Melano',
+                           'Gestora'
+                       );
+
+
+-- Tabla: Solicitud
+CREATE TABLE IF NOT EXISTS Solicitud (
+    ID_solicitud    INTEGER PRIMARY KEY AUTOINCREMENT,
+    ID_user         INTEGER REFERENCES Usuarios (ID),
+    ID_documento    INTEGER REFERENCES Documentos (ID_documento),
+    ID_encargado    INTEGER REFERENCES Encargados (ID_encargado),
+    Fecha_solicitud REAL,
+    Estado          TEXT    DEFAULT Finalizado
+                            DEFAULT Proceso
+                            DEFAULT Entregado
+);
+
+
+-- Tabla: Usuarios
+CREATE TABLE IF NOT EXISTS Usuarios (
+    ID              INTEGER PRIMARY KEY AUTOINCREMENT
+                            UNIQUE,
+    Nombres         TEXT,
+    Apellido        TEXT,
+    Correo          TEXT,
+    Fecha_registro  REAL,
+    contraseña_hash TEXT
+);
+
+INSERT INTO Usuarios (
+                         ID,
+                         Nombres,
+                         Apellido,
+                         Correo,
+                         Fecha_registro,
+                         contraseña_hash
+                     )
+                     VALUES (
+                         1,
+                         'Jhoan',
+                         'Cuesta',
+                         'jhoancuesta034@gmail.com',
+                         '4/10/2024',
+                         'Jhoan2006'
+                     );
+
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
---
+++ Sprint 3/Jhoan Cuesta - Bases de Datos/Practica/SQLite/One_window_datausers.sql
@@ -0,0 +1,17 @@
+--
+-- Archivo generado con SQLiteStudio v3.4.4 el dom. oct. 6 20:15:26 2024
+--
+-- Codificación de texto usada: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Tabla: Data_Users
+CREATE TABLE IF NOT EXISTS Data_Users (
+    Usuario       NUMERIC,
+    [ Contraseña] TEXT
+);
+
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
